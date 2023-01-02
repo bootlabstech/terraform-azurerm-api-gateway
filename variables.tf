7af482252c40b24a6584666241a2421c468dce29
lalitha2407 <lalitha@bootlabstech.com>
2023-01-02 12:13:29 +0530

    added the network configuration

--- variables.tf
+++ variables.tf
@@ -22,3 +22,11 @@ variable "sku_name" {
   type        = string
   description = "SKU name the gateway."
 }
+variable "subnet_id" {
+  type        = string
+  description = "The subnet where the API gateway will be created in."
+}
+variable "virtual_network_type" {
+  type        = string
+  description = "The type of virtual network you want to use, valid values include: None, External, Internal"
+}
