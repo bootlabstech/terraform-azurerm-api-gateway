e856ffed29e5701e86607c07024ecc5f17b1a00c
lalitha2407 <lalitha@bootlabstech.com>
2023-02-01 17:59:27 +0530

    changed network config

--- main.tf
+++ main.tf
@@ -5,18 +5,26 @@ resource "azurerm_api_management" "example" {
   publisher_name      = var.publisher_name
   publisher_email     = var.publisher_email
   sku_name = var.sku_name
-}
-resource "azurerm_private_endpoint" "endpoint" {
-  name                = var.private_endpoint_name
-  location            = var.location
-  resource_group_name = var.private_endpoint_resource_group_name
-  subnet_id           = var.private_endpoint_subnet_id
+  virtual_network_type = var.virtual_network_type
+  virtual_network_configuration {
 
-  private_service_connection {
-    name                           = "${var.private_endpoint_name}-connection"
-    private_connection_resource_id = azurerm_api_management.example.id
-    is_manual_connection           = false
-    subresource_names              = var.subresource_names
+    subnet_id = var.subnet_id
   }
-
+  
 }
+
+
+# resource "azurerm_private_endpoint" "endpoint" {
+#   name                = var.private_endpoint_name
+#   location            = var.location
+#   resource_group_name = var.private_endpoint_resource_group_name
+#   subnet_id           = var.private_endpoint_subnet_id
+
+#   private_service_connection {
+#     name                           = "${var.private_endpoint_name}-connection"
+#     private_connection_resource_id = azurerm_api_management.example.id
+#     is_manual_connection           = false
+#     subresource_names              = var.subresource_names
+#   }
+
+# }
--- variables.tf
+++ variables.tf
@@ -22,32 +22,42 @@ variable "sku_name" {
   type        = string
   description = "SKU name the gateway."
 }
-variable "private_endpoint_subnet_id" {
+# variable "private_endpoint_subnet_id" {
+#   type        = string
+#   description = "The subnet where the API gateway will be created in."
+# }
+# variable "private_endpoint_name" {
+#   type = string
+#   description = "Name of the private endpoint used by the api gateway"
+  
+# }
+# variable "private_endpoint_resource_group_name" {
+#   type = string
+#   description = "The resource group under which the endpoint will get created"
+  
+# }
+# variable "private_connection_resource_id" {
+#   type = string
+#   description = "The id of the private endpoint resource"
+  
+# }
+# variable "is_manual_connection" {
+#   type = bool
+#   description = "Manual connection or not"
+  
+# }
+# variable "subresource_names" {
+#   type = list(string)
+#   description = "subresource residing in the endpoint"
+  
+# }
+
+
+variable "subnet_id" {
   type        = string
   description = "The subnet where the API gateway will be created in."
 }
-variable "private_endpoint_name" {
-  type = string
-  description = "Name of the private endpoint used by the api gateway"
-  
-}
-variable "private_endpoint_resource_group_name" {
-  type = string
-  description = "The resource group under which the endpoint will get created"
-  
-}
-variable "private_connection_resource_id" {
-  type = string
-  description = "The id of the private endpoint resource"
-  
-}
-variable "is_manual_connection" {
-  type = bool
-  description = "Manual connection or not"
-  
-}
-variable "subresource_names" {
-  type = list(string)
-  description = "subresource residing in the endpoint"
-  
+variable "virtual_network_type" {
+  type        = string
+  description = "The type of virtual network you want to use, valid values include: None, External, Internal"
 }
